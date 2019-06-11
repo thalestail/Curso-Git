1feb9e5a38e046e79348cb7325fc9a9c45b16536
Thales Lima <thalestail@hotmail.com>
2019-06-11 16:40:08 -0300

    Adicionando a função de divisão

--- projetoX.c
+++ projetoX.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-
+int div (int a, int b){
+	int div_num = a/b;
+	return div_num;
+}
 int mult (int a, int b){
 	int mult_num = a*b;
 	return mult_num;
@@ -18,5 +21,6 @@ int main (){
 	printf("Soma dos numeros: %d\n", soma (3,4));
 	printf("Subtracao dos numeros: %d\n", sub (5,3));
 	printf("Multiplicação dos numeros: %d\n", mult (4,5));
+	printf("Divisao dos numeros: %d\n", div (10,5));
 	return 0;
 }
